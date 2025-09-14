cf2e2ba9cc08284f155b2aeb91a4211fe9c70a91
unknown <drishya1912@gmail.com>
2025-09-14 20:58:23 +0530

    using pysqlite

--- src/retriever.py
+++ src/retriever.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 """Retriever: embeddings + Chroma vectorstore setup."""
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
